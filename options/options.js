a4377d1c82c3bf0433f7b02352b6e756b30269af
Dave.o Olund <dave.o@crowdbotics.com>
2023-07-28 17:21:49 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,5 +1,3 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "dave20230728-dev-105121",
   url: "https://dave20230728-dev-105121.botics.co",
@@ -7,9 +5,9 @@ export const globalOptions = {
   screenOptions: {
     headerShown: false
   }
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
